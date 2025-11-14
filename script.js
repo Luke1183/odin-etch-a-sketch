a3c795c96bb6ceab71911c85322d068a9b1b45a0
Luke <141526416+Luke1183@users.noreply.github.com>
2025-11-14 16:39:26 +0100

    Setup initial files and grid size button logic

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
 // Create a 16x16 grid here
+function removeGrid() {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+}
+
 function createGrid(n) {
   for (let i = 1; i <= n; i++) {
-    const cell = "cell" + i;
+    let cell = "cell" + i;
     cell = document.createElement("div");
     cell.textContent = `${i}`;
     gridContainer.appendChild(cell);
   }
 }
-const gridContainer = document.querySelector("#grid-container");
+
+const gridSizeButton = document.querySelector("#grid-size");
+gridSizeButton.addEventListener("click", () => {
+  let userInput = Number(prompt("Enter a number between 16 and 100:"));
+  let errorMessage = "";
+
+  if (Number.isInteger(userInput)) {
+    if (userInput > 15 && userInput < 101) {
+      n = userInput;
+      createGrid(n);
+    } else {
+      removeGrid();
+      errorMessage = "Number not within range";
+    }
+  } else {
+    removeGrid();
+    errorMessage = "Must enter a number";
+  }
+  if (errorMessage === "") {
+    removeGrid();
+    createGrid(n);
+  } else {
+    errorMessageText = document.createElement("div");
+    errorMessageText.textContent = errorMessage;
+    gridContainer.appendChild(errorMessageText);
+  }
+});
+
+let errorMessage = "";
 let n = 16;
+const gridContainer = document.querySelector("#grid-container");
 createGrid(n);
